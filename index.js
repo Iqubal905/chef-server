55277fab8aa5a5f128ea53165faf7433ebfa90e7
Iqubal905 <iqubal905@gmail.com>
2023-05-01 22:43:14 +0600

    apiMaked

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+// const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+
+const recipes = require('./data/recipes.json');
+
+app.get('/', (req, res) => {
+    res.send('Site is running')
+});
+
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
+})
+
+app.listen(port, () => {
+    console.log(`Site API is running on port: ${port}`)
+})
